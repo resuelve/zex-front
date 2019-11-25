85b43f1373d9ad97a28732234bd8adb360e11b46
Carlos Flores <carlos.flores@resuelve.mx>
2019-11-25 17:17:34 -0600

    Add user search component

---
+++ src/app/components/shared/searchCoworker.vue
@@ -0,0 +1,78 @@
+<style lang="sass" scoped>
+.searchCoworker
+  position: relative
+  input
+    width: 100%
+    border: none
+    border-bottom: 1px solid var(--titanium)
+    font-size: 14px
+    padding: 0 4px
+    background-color: transparent
+    color: var(--lighter-gray-bg)
+    &::-webkit-input-placeholder
+      color: rgba(var(--lighter-gray-bg), .5)
+    &:not(:placeholder-shown)
+      + .optionBox
+        display: inherit
+  .optionBox
+    display: none
+    position: absolute
+    width: 100%
+    background: var(--blueberry) linear-gradient(135deg, rgba(#ffffff, 0.2) 0%, rgba(#ffffff, 0) 100%)
+    box-shadow: 5px 5px 10px -5px var(--pinot-noir)
+    margin-top: 5px
+    font-weight: 400
+    text-transform: none
+  li
+    cursor: pointer
+    display: flex
+    align-items: center
+    padding: 0 5px
+    &:hover
+      background-color: var(--titanium)
+      color: var(--pinot-noir)
+    img
+      margin-right: 6px
+</style>
+
+<script>
+import users from 'mocks/users.json'
+import userPicture from '~/shared/userPicture.vue'
+export default {
+  data: () => ({
+    searchValue: '',
+  }),
+  methods: {
+    selectOption (user) {
+      this.$emit('coworkerSelected', user)
+      this.searchValue = ''
+    }
+  },
+  computed: {
+    users () {
+      if(this.searchValue.length < 3) return
+      const sV = this.searchValue.toLowerCase()
+      return users.filter(u => {
+        return u.email.toLowerCase().indexOf(sV) !== -1 ||
+          u.name.toLowerCase().indexOf(sV) !== -1 ||
+          u.nickname.toLowerCase().indexOf(sV) !== -1
+      })
+    }
+  },
+  components: {
+    userPicture
+  }
+}
+</script>
+
+<template lang="pug">
+  .searchCoworker
+    input(v-model="searchValue", placeholder="Buscar colaborador")
+    ul.optionBox
+      li(
+        v-for="user in users"
+        @click="selectOption(user)"
+      )
+        userPicture.small(:src="user.avatar")
+        |{{user.nickname}}
+</template>
---
+++ src/app/components/shared/userPicture.vue
@@ -0,0 +1,28 @@
+<style lang="sass" scoped>
+img
+  width: 30px
+  height: 30px
+  border-radius: 50%
+  object-fit: cover
+  &.small
+    width: 15px
+    height: 15px
+  &.big
+    width: 45px
+    height: 45px
+</style>
+<template lang="pug">
+  img(
+    v-if="src"
+    :src="src"
+  )
+</template>
+<script>
+export default {
+  props: {
+    src: {
+      default: false
+    }
+  }
+}
+</script>
